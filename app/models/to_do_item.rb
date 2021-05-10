e53195228ec5c20242935281e8022d71f4793937
ambasuthanjananayagam <ambasuthanjananayagam@yahoo.co.uk>
2021-05-10 13:33:54 +0100

    restful process - homework

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -1,2 +1,5 @@
 class ToDoItem < ApplicationRecord
+  attr_accessor :title, :description, :status
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-
-
-get '/todoitem/:id', to: 'todoitem#show', as: 'show'
-get '/todoitems', to: 'todoitem#index', as: 'index'
-
-
+    get '/todoitem/:id', to: 'todoitem#show', as: 'show'
+    get '/todoitems', to: 'todoitem#index', as: 'index'
+    get '/todoitems/new', to: 'todoitem#new', as: 'new'
+    post '/todoitems/new', to: 'todoitem#create', as: 'create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
